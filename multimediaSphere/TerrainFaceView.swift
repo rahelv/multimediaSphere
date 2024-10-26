1187e6dff81fb833b9354504abb0da48d183deeb
Rahel <V>
2024-10-26 15:15:53 +0200

    add texture mapping

--- multimediaSphere/TerrainFaceView.swift
+++ multimediaSphere/TerrainFaceView.swift
@@ -20,12 +20,12 @@ struct TerrainFaceView: View {
         }
         .task {
             //TODO: create richtungen und dann createTerrainFace pro Richtung
-//            await createTerrainFace(resolution: 10, localUp: .init(x: 0, y: 1, z: 0)) //up
-//            await createTerrainFace(resolution: 10, localUp: .init(x: 0, y: -1, z: 0)) //down
-//            await createTerrainFace(resolution: 10, localUp: .init(x: 1, y: 0, z: 0)) //right
-//            await createTerrainFace(resolution: 10, localUp: .init(x: -1, y: 0, z: 0)) //left
-            await createTerrainFace(resolution: 3, localUp: .init(x: 0, y: 0, z: 1)) //front
-//            await createTerrainFace(resolution: 10, localUp: .init(x: 0, y: 0, z: -1)) //back
+            await createTerrainFace(resolution: 10, localUp: .init(x: 0, y: 1, z: 0)) //up
+            await createTerrainFace(resolution: 10, localUp: .init(x: 0, y: -1, z: 0)) //down
+            await createTerrainFace(resolution: 10, localUp: .init(x: 1, y: 0, z: 0)) //right
+            await createTerrainFace(resolution: 10, localUp: .init(x: -1, y: 0, z: 0)) //left
+            await createTerrainFace(resolution: 10, localUp: .init(x: 0, y: 0, z: 1)) //front
+            await createTerrainFace(resolution: 10, localUp: .init(x: 0, y: 0, z: -1)) //back
         }
     }
     
@@ -57,64 +57,71 @@ struct TerrainFaceView: View {
         let axisA: SIMD3<Float> = SIMD3<Float>(localUp.y, localUp.z, localUp.x) //cyclic permutation (rechtwinklig zu localUp)
         let axisB: SIMD3<Float> = simd_cross(localUp, axisA) //senkrecht auf localUp and axisA
         
-        var vertices = [SIMD3<Float>](repeating: .zero, count: resolution * resolution) //vertex for each "edge" on the sphere
-        var quads = [UInt32](repeating: 0, count: (resolution - 1) * (resolution - 1) * 4) //faces
-        var textureCoordinates = [SIMD2<Float>](repeating: .zero, count: (resolution - 1) * (resolution - 1) * 4) //texture coordinates corresponding to faces
+        var vertexPositions = [SIMD3<Float>](repeating: .zero, count: resolution * resolution)//vertex for each "edge" on the sphere
 
-        var quadIndex = 0
+        var verticesWithDuplicates = [SIMD3<Float>]() //vertex for each "edge" on the sphere
+        var quads = [UInt32]() //faces
+        var textureCoordinates = [SIMD2<Float>]() //texture coordinates corresponding to faces
         
+        var vertex_index = 0
+
         for y in 0..<resolution { //row
             for x in 0..<resolution { //column
                 let i = x + y * resolution //index to store 2d array in linear order. row-major order
                 let percent = SIMD2<Float>(Float(x), Float(y)) / Float(resolution - 1) //TODO
-//                print("index \(i)")
-//                print("print \(percent)")
-
+                
                 let normalizedCoordinateX = (percent.x - 0.5) * 2
                 let normalizedCoordinateY = (percent.y - 0.5) * 2
                 
                 let tangentAOffset = normalizedCoordinateX * axisA
                 let tangentBOffset = normalizedCoordinateY * axisB
                 
+                //TODO: remove
                 let pointOnUnitCube = localUp + tangentAOffset + tangentBOffset
-                addSphere(position: pointOnUnitCube, color: .blue)
-
+//                addSphere(position: pointOnUnitCube, color: .blue)
+                
                 let pointOnUnitSphere = simd_normalize(pointOnUnitCube)
                 addSphere(position: pointOnUnitSphere, color: .red)
                 
-//                vertices[i] = pointOnUnitSphere
-                vertices[i] = pointOnUnitCube
-
+                vertexPositions[i] = pointOnUnitSphere
+//                vertexPositions[i] = pointOnUnitCube
+            }
+        }
+        
+        for y in 0..<resolution { //row
+            for x in 0..<resolution { //column
+                let i = x + y * resolution //index to store 2d array in linear order. row-major order
                 guard (x != resolution - 1 && y != resolution - 1) else { continue }
-//                if (x == 0 && y == 0) {
-                    quads[quadIndex] = UInt32(i)
-                    quads[quadIndex + 1] = UInt32(i + 1)
-                    quads[quadIndex + 2] = UInt32(i + resolution + 1)
-                    quads[quadIndex + 3] = UInt32(i + resolution)
-                    print("next\n")
-                    print("quads \(quads)")
-                    print("next\n")
+ //                if (x == 0 && y == 0) {
+                //first add new vertices (with duplicates)
+                //TODO: there has to be a much more efficient way ...
+                verticesWithDuplicates.append(vertexPositions[i])
+                verticesWithDuplicates.append(vertexPositions[i + 1])
+                verticesWithDuplicates.append(vertexPositions[i + resolution + 1])
+                verticesWithDuplicates.append(vertexPositions[i + resolution])
+                
+                //then add faces
+                quads.append(UInt32(vertex_index))
+                quads.append(UInt32(vertex_index + 1))
+                quads.append(UInt32(vertex_index + 2))
+                quads.append(UInt32(vertex_index + 3))
+                
+                vertex_index += 4
 
-                    textureCoordinates[quadIndex] = [0, 0]
-                    textureCoordinates[quadIndex + 1] = [1, 0]
-                    textureCoordinates[quadIndex + 2] = [1, 1]
-                    textureCoordinates[quadIndex + 3] = [0, 1]
-                    print("textureCoords \(textureCoordinates)")
-                    quadIndex += 4
+                textureCoordinates.append([0, 0])
+                textureCoordinates.append([1, 0])
+                textureCoordinates.append([1, 1])
+                textureCoordinates.append([0, 1])
+//              print("textureCoords \(textureCoordinates)")
 //                }
             }
         }
-        print("next\n")
-        print("vertices \(vertices)")
-        print("textureCoords COUNT \(textureCoordinates.count)")
-        print("vertices COUNT \(vertices.count)")
 
         let size = (resolution - 1) * (resolution - 1)
         var descriptor = MeshDescriptor(name: "face")
-        descriptor.positions = MeshBuffers.Positions(vertices)
+        descriptor.positions = MeshBuffers.Positions(verticesWithDuplicates)
         descriptor.primitives = .polygons(Array(repeating: 4, count: size), quads) //TODO: ist das sinnvoll? (triangles and quads maybe??)
-        print("primitives \(descriptor.primitives)")
-//        descriptor.textureCoordinates = MeshBuffer.init(textureCoordinates)
+        descriptor.textureCoordinates = MeshBuffer.init(textureCoordinates)
         let materialsArray: [UInt32] = Array(0..<size).map { UInt32($0) } //TODO
         descriptor.materials = .perFace(materialsArray)
         return descriptor
