51b4cad6e160bc49fb3eedbda33fe99d40b34287
Rahel <V>
2024-11-04 23:14:54 +0100

    refactor material generator

--- multimediaSphere/Sphere/ImageMaterialGenerator.swift
+++ multimediaSphere/Sphere/ImageMaterialGenerator.swift
@@ -7,15 +7,16 @@
 
 import Foundation
 import RealityFoundation
+import UIKit
 
 struct ImageMaterialGenerator {
     // TODO: load materials from web
-    static func generateMaterials() -> [SimpleMaterial] {
+    static func generateMaterials(count: Double) -> [SimpleMaterial] {
     //  var textures: [TextureResource] = [] //TODO: why differentiate between materials and textures?
         var materials: [SimpleMaterial] = []
         
         // Loop to load textures dynamically
-        for i in 1 ... 81 {
+        for i in 1 ... Int(count) {
             let textureName = String(format: "image_%05d", i)
             do {
                 let texture = try TextureResource.load(named: textureName)
@@ -31,4 +32,38 @@ struct ImageMaterialGenerator {
         }
         return materials
     }
+    
+    func loadImageFromURL(_ url: URL, completion: @escaping (UIImage?) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                completion(image)
+            }
+        }.resume()
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//             detail = indexPath.item + 1
+//             //let imageUrlString = "http://10.34.58.149/Data/CLS-LOC/train/n03832673/n03832673_17810.JPEG"
+//             if let results = results, indexPath.item < results.count {
+//                 var imageUrlString : String = ""
+//                 if (results[indexPath.item].starts(with: "[")){
+//                     let path = results[indexPath.item].dropFirst()
+//                     imageUrlString = endpoint + path.dropFirst().dropLast()
+//                 } else {
+//                     imageUrlString = endpoint + results[indexPath.item].dropFirst().dropLast()
+//                 }
+//                 if let imageUrl = URL(string: imageUrlString) {
+//                     loadImageFromURL(imageUrl) { [weak self] image in
+//                         self?.detailImageView.image = image
+//                         self?.toggleDetailVisibility()
+//                     }
+//                 }
+//             }
+//         }
 }
